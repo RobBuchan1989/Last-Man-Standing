2f62d1623cf041a727f4ae9120a0557c213b6b2b
RobBuchan1989 <robertbuchan@hotmail.co.uk>
2026-09-11 09:27:52 +0100

    Update teams.ts

--- lib/teams.ts
+++ lib/teams.ts
@@ -6,37 +6,149 @@ export type Team = {
   crest: string
 }
 
-const ids: Record<string, [number, string, string]> = {
-  "Arsenal": [57, "Arsenal", "ARS"],
-  "Aston Villa": [58, "Aston Villa", "AVL"],
-  "Bournemouth": [1044, "Bournemouth", "BOU"],
-  "Brentford": [337, "Brentford", "BRE"],
-  "Brighton & Hove Albion": [397, "Brighton", "BHA"],
-  "Chelsea": [61, "Chelsea", "CHE"],
-  "Crystal Palace": [354, "Crystal Palace", "CRY"],
-  "Everton": [62, "Everton", "EVE"],
-  "Fulham": [63, "Fulham", "FUL"],
-  "Leeds United": [341, "Leeds United", "LEE"],
-  "Liverpool": [64, "Liverpool", "LIV"],
-  "Manchester City": [65, "Manchester City", "MCI"],
-  "Manchester United": [66, "Manchester United", "MUN"],
-  "Newcastle United": [67, "Newcastle", "NEW"],
-  "Nottingham Forest": [351, "Nottingham Forest", "NFO"],
-  "Sunderland": [71, "Sunderland", "SUN"],
-  "Tottenham Hotspur": [73, "Tottenham", "TOT"],
+const teamsData: Team[] = [
+  {
+    id: 57,
+    name: "Arsenal",
+    shortName: "Arsenal",
+    tla: "ARS",
+    crest: "https://crests.football-data.org/57.png",
+  },
+  {
+    id: 58,
+    name: "Aston Villa",
+    shortName: "Aston Villa",
+    tla: "AVL",
+    crest: "https://crests.football-data.org/58.png",
+  },
+  {
+    id: 1044,
+    name: "Bournemouth",
+    shortName: "Bournemouth",
+    tla: "BOU",
+    crest: "https://crests.football-data.org/1044.png",
+  },
+  {
+    id: 337,
+    name: "Brentford",
+    shortName: "Brentford",
+    tla: "BRE",
+    crest: "https://crests.football-data.org/337.png",
+  },
+  {
+    id: 397,
+    name: "Brighton & Hove Albion",
+    shortName: "Brighton",
+    tla: "BHA",
+    crest: "https://crests.football-data.org/397.svg",
+  },
+  {
+    id: 61,
+    name: "Chelsea",
+    shortName: "Chelsea",
+    tla: "CHE",
+    crest: "https://crests.football-data.org/61.png",
+  },
+  {
+    id: 354,
+    name: "Crystal Palace",
+    shortName: "Crystal Palace",
+    tla: "CRY",
+    crest: "https://crests.football-data.org/354.png",
+  },
+  {
+    id: 62,
+    name: "Everton",
+    shortName: "Everton",
+    tla: "EVE",
+    crest: "https://crests.football-data.org/62.png",
+  },
+  {
+    id: 63,
+    name: "Fulham",
+    shortName: "Fulham",
+    tla: "FUL",
+    crest: "https://crests.football-data.org/63.png",
+  },
+  {
+    id: 341,
+    name: "Leeds United",
+    shortName: "Leeds United",
+    tla: "LEE",
+    crest: "https://crests.football-data.org/341.png",
+  },
+  {
+    id: 64,
+    name: "Liverpool",
+    shortName: "Liverpool",
+    tla: "LIV",
+    crest: "https://crests.football-data.org/64.png",
+  },
+  {
+    id: 65,
+    name: "Manchester City",
+    shortName: "Manchester City",
+    tla: "MCI",
+    crest: "https://crests.football-data.org/65.png",
+  },
+  {
+    id: 66,
+    name: "Manchester United",
+    shortName: "Manchester United",
+    tla: "MUN",
+    crest: "https://crests.football-data.org/66.png",
+  },
+  {
+    id: 67,
+    name: "Newcastle United",
+    shortName: "Newcastle",
+    tla: "NEW",
+    crest: "https://crests.football-data.org/67.png",
+  },
+  {
+    id: 351,
+    name: "Nottingham Forest",
+    shortName: "Nottingham Forest",
+    tla: "NFO",
+    crest: "https://crests.football-data.org/351.png",
+  },
+  {
+    id: 71,
+    name: "Sunderland",
+    shortName: "Sunderland",
+    tla: "SUN",
+    crest: "https://crests.football-data.org/71.png",
+  },
+  {
+    id: 73,
+    name: "Tottenham Hotspur",
+    shortName: "Tottenham",
+    tla: "TOT",
+    crest: "https://crests.football-data.org/73.png",
+  },
+  {
+    id: 1076,
+    name: "Coventry City",
+    shortName: "Coventry City",
+    tla: "COV",
+    crest: "https://crests.football-data.org/1076.png",
+  },
+  {
+    id: 349,
+    name: "Ipswich Town",
+    shortName: "Ipswich Town",
+    tla: "IPS",
+    crest: "https://crests.football-data.org/349.png",
+  },
+  {
+    id: 322,
+    name: "Hull City",
+    shortName: "Hull City",
+    tla: "HUL",
+    crest: "https://crests.football-data.org/322.png",
+  },
+]
 
-  // Promoted for 2026/27
-  "Coventry City": [1076, "Coventry City", "COV"],
-  "Ipswich Town": [349, "Ipswich Town", "IPS"],
-  "Hull City": [322, "Hull City", "HUL"],
-}
-
-export const teams: Team[] = Object.entries(ids)
-  .map(([name, [id, shortName, tla]]) => ({
-    id,
-    name,
-    shortName,
-    tla,
-    crest: `https://crests.football-data.org/${id}.png`,
-  }))
-  .sort((a, b) => a.name.localeCompare(b.name))
+export const teams: Team[] = teamsData.sort((a, b) =>
+  a.name.localeCompare(b.name)
+)
