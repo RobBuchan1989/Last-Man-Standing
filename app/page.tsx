a5479ebc1afeba0bde8437122acc3b6f251fc1c1
RobBuchan1989 <robertbuchan@hotmail.co.uk>
2026-09-22 09:27:03 +0100

    Use local lion watermark asset for reliable deployment

--- app/page.tsx
+++ app/page.tsx
@@ -307,10 +307,10 @@ async function HomePage() {
         <div aria-hidden="true" className="absolute inset-0 -z-10 bg-[linear-gradient(180deg,rgba(3,10,17,0.24)_0%,rgba(3,10,17,0.06)_55%,rgba(3,10,17,0.82)_100%)]" />
 
         <img
-          src="https://logo.premierleague.com/img/lion-light.svg"
+          src="/premier-league-lion-watermark.svg"
           alt=""
           aria-hidden="true"
-          className="pointer-events-none absolute right-[10%] top-1/2 -z-10 hidden h-[22rem] w-[18rem] -translate-y-1/2 object-contain opacity-[0.09] sm:block lg:right-[12%] lg:h-[28rem] lg:w-[23rem]"
+          className="pointer-events-none absolute right-[7%] top-1/2 -z-10 hidden h-[15rem] w-[12rem] -translate-y-1/2 object-contain opacity-[0.08] sm:block lg:right-[9%] lg:h-[20rem] lg:w-[16rem]"
         />
 
         <div className="mx-auto max-w-7xl px-4 pb-10 pt-5 sm:px-6 sm:pb-14 sm:pt-7 lg:px-8">
@@ -1404,10 +1404,10 @@ async function LeaguePage({
         <div aria-hidden="true" className="absolute inset-0 -z-10 bg-[linear-gradient(180deg,rgba(3,10,17,0.24)_0%,rgba(3,10,17,0.06)_55%,rgba(3,10,17,0.82)_100%)] />
 
         <img
-          src="https://logo.premierleague.com/img/lion-light.svg"
+          src="/premier-league-lion-watermark.svg"
           alt=""
           aria-hidden="true"
-          className="pointer-events-none absolute right-[10%] top-1/2 -z-10 hidden h-[22rem] w-[18rem] -translate-y-1/2 object-contain opacity-[0.09] sm:block lg:right-[12%] lg:h-[28rem] lg:w-[23rem]"
+          className="pointer-events-none absolute right-[7%] top-1/2 -z-10 hidden h-[15rem] w-[12rem] -translate-y-1/2 object-contain opacity-[0.08] sm:block lg:right-[9%] lg:h-[20rem] lg:w-[16rem]"
         />
 
         <div className="mx-auto max-w-7xl px-4 pb-7 pt-5 sm:px-6 sm:pb-10 sm:pt-7 lg:px-8">
